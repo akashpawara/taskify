b508c4981e79c9fc217ae31414055f81dd87dbf9
Akash Pawara <akashpawara29@gmail.com>
2020-10-30 22:35:11 +0530

    app vue created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue'
+import App from './app.vue'
 
-createApp(App).mount('#app')
+import 'semantic-ui-css/semantic.css';
+import router from './router'
+
+
+new Vue({
+  router,
+  render: h => h(App),
+}).$mount('#app')
